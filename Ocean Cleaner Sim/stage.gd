f38044f3dbb181522211ea4d71d367ae835f79d6
Jellytots04 <116292161+Jellytots04@users.noreply.github.com>
2025-11-13 19:23:51 +0000

    Previous push didnt go through
    This was the previous push that didnt go through

--- Ocean Cleaner Sim/stage.gd
+++ Ocean Cleaner Sim/stage.gd
@@ -1,13 +1,16 @@
 extends Node3D
 
 @export var trash_pile: PackedScene
+@export var liquid_spill: PackedScene
 @onready var spawn_floor: MeshInstance3D = $SpawnFloor
 
 # Will be used to hold the values
 var currentTrash = []
+var currentLiquids = []
 
 # Limit to how much trash can be in the water
 var trashLimit = 5
+var liquidLimit = 3
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
@@ -28,9 +31,23 @@ func spawn_pickable():
 	currentTrash.append(instance)
 	add_child(instance)
 
+func spawn_liquid():
+	var aabb = spawn_floor.mesh.get_aabb()
+	var global_origin = spawn_floor.global_transform.origin
+	var x = randf_range(global_origin.x + aabb.position.x, global_origin.x + aabb.position.x + aabb.size.x)
+	var z = randf_range(global_origin.z + aabb.position.z, global_origin.z + aabb.position.z + aabb.size.z)
+	var y = 0.1
+	var instance = liquid_spill.instantiate()
+	instance.global_position = Vector3(x, y, z)
+	currentLiquids.append(instance)
+	add_child(instance)
 
 func _on_spawn_timer_timeout() -> void:
 	if trashLimit > currentTrash.size():
 		spawn_pickable()
 	else: 
 		print("Limit has been reached")
+	if liquidLimit > currentLiquids.size():
+		spawn_liquid()
+	else:
+		print("Liquid limit reached")
