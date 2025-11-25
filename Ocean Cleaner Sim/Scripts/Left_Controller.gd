7782a51b379d2643935900c67a500b6caad3ef16
Jellytots04 <116292161+Jellytots04@users.noreply.github.com>
2025-11-25 18:02:12 +0000

    Added tween to bring bag to user
    Using a tween to bring the bag to the user and have the bag be in the scene automatically, when the player presses the left grip the bag automatically gets tweened towards the players controller.

--- Ocean Cleaner Sim/Scripts/Left_Controller.gd
+++ Ocean Cleaner Sim/Scripts/Left_Controller.gd
@@ -1,15 +1,30 @@
 extends XRController3D
 
-@export var bagUI = "res://UIController.tscn"
-@export var spawn_distance := 1.0
+@onready var bagUI = $"../../StaticBody3D"
+@onready var bag = $"../../BagStatic"
+@export var spawn_distance := 5.0
+
+var bagUI_base_pos
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
+	print(bag)
+	print(bagUI)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if is_button_pressed("ax_button"):
-		var spawn_pos = global_transform.origin + -global_transform.basis.z * spawn_distance
-		bagUI.global_transform.origin = spawn_pos
+		pass
+
+	if is_button_pressed("grip_click"):
+		var bag_offset = Vector3(0, -0.2, 0.2)
+		var offset_position = global_transform.origin + global_transform.basis * bag_offset
+		var tween = bag.create_tween()
+		tween.set_parallel(true)
+		tween.tween_property(bag, "global_position", offset_position, 1.0).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
+		tween.tween_property(bag, "rotation", -self.rotation, 0.1)
+		
+#
+#tween.tween_property($Sprite, "position:y", 400, 1.0)\
+		#.set_trans(Tween.TRANS_BOUNCE)\
+		#.set_ease(Tween.EASE_OUT)
