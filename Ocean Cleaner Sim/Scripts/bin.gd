2e0798c3cabf91528ed6680037a81df9227dfd1c
Jellytots04 <116292161+Jellytots04@users.noreply.github.com>
2025-12-05 19:28:23 +0000

    Edited bin script, added character script
    Changed trashbins layer and mask to read in the bag and trash.
    
    bin script now has 2 signals set up for adding points and emptying the bag, add points will be sent to the character script, which will then hold the number of points and this value will increase.
    
    The signal empty bag will be sent to the bag to clear its content setting it to 0 and setting the progress bar back down to 0.

--- Ocean Cleaner Sim/Scripts/bin.gd
+++ Ocean Cleaner Sim/Scripts/bin.gd
@@ -1,15 +1,25 @@
 extends StaticBody3D
 
+signal addPoints
+signal emptyBag
+
+var bag
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	bag = get_node("/root/Main/BagStatic")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	pass # Replace with function body.
+	print(body)
+	if body == bag:
+		print("This is the bag yippee")
+
+func pointAdd():
+	emit_signal("addPoints")
+
+func clearBagItems():
+	emit_signal("emptyBag")
