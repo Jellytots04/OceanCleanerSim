7df704d63c3910850e9e12487365e0cce1da9e4d
Jellytots04 <jello352@gmail.com>
2025-12-16 22:12:56 +0000

    Created functions for bobbing animations on yacht

--- Ocean Cleaner Sim/Scenes/Yacht_Script.gd
+++ Ocean Cleaner Sim/Scenes/Yacht_Script.gd
@@ -1,1 +1,18 @@
 extends Node3D
+
+var spawn_pos
+@onready var yacht = $Yacht
+@onready var animation = $AnimationPlayer
+
+func _ready() -> void:
+	spawn_yacht()
+
+# Spawn it at -50 and tween it to 0 z
+func spawn_yacht():
+	spawn_pos = global_position.z - 50
+
+func move_animation():
+	animation.play("YachtBob")
+
+func stationed():
+	animation.play("bobstationed")
