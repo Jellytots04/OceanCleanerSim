18941ec07c3ffb9d29eb1e2cafa99170cae95fd0
Jellytots04 <116292161+Jellytots04@users.noreply.github.com>
2025-12-17 07:44:09 +0000

    Added in multiplier and added it to UI wall

--- Ocean Cleaner Sim/Scripts/bin.gd
+++ Ocean Cleaner Sim/Scripts/bin.gd
@@ -4,13 +4,16 @@ signal addPoints
 signal emptyBag
 
 var bag
+var multiplier
+var multiplierContent
 
 @onready var fire = $Fire
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	bag = get_node("/root/Main/BagStatic")
-	
+	multiplier = get_node("/root/Main/Stage/UI Wall/Multiplier/Viewport2Din3D/Viewport")
+	multiplierContent = multiplier.get_child(0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
